f506325c48ad1753162769e15d159b600dfcfcf4
Akash <150268192+akash2904r@users.noreply.github.com>
2024-02-02 07:46:19 +0530

    Add files via upload

---
+++ Tutorial101 - X Clone using Tailwind CSS/My Solution/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    fontFamily: {
+      blink: ['Blinker', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
