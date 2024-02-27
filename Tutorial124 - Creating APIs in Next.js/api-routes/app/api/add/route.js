03bbb9f463a806a60b20692d37319925cd0a4c7d
Akash <150268192+akash2904r@users.noreply.github.com>
2024-02-27 18:05:53 +0530

    Delete Tutorial124 - Creating APIs in Next.js/api-routes directory

--- Tutorial124 - Creating APIs in Next.js/api-routes/app/api/add/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export async function POST(request) {
-    let data = await request.json()
-    console.log(data)
-    return NextResponse.json({ success: true, data })
-}
